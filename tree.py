5e135b9cc0090f6c6424ead6ac3543e8721a8e8d
lipc <3236568459@qq.com>
2017-06-22 12:13:44 +0800

    ok to use figer out pre version problem 输出的目录树是正常的了。 但是不知道怎么才可以 做的和tree工具一样的方便调用。 这个应该只是会在用tree输出中文乱码之后才会用到的吧。

--- tree.py
+++ tree.py
@@ -1,53 +1,63 @@
 #!/usr/local/bin/python3.6
 
 import os
-rootname = "/Users/luo-banqi/Desktop" 
-#for x in os.walk(rootname):
-#	print(x)
-#print(os.listdir(rootname))
-
+#rootname = "/Users" 
 
 
 # 输出这个路径下的 内容。
-def tree(path,deep=0,maxdeep=5,show_invisibale=False):
+def tree(path,show_invisibale=False,deep=0,maxdeep=5,pre='├──'):
 	if(deep==0):
 		print(path)
 	if(deep==maxdeep):
 		return
-	pre = deep*'│   '+'├── '
-
-	pre2= deep*'│   '+'└── '
+	
 
 	files = os.listdir(path)
 	last_out =[]
 
-	# x 是相对路径。
+	# x 是相对路径。 把里面的文件和文件夹分离。 文件夹最后输出。先放到last_out里面。
 	for x in files:
-		abspath = os.path.join(path,x)
-		if((x[0]=='.' or x[0]=='~') and not show_invisibale):
+		if((x[0]=='.' or x[0]=='~' or x[0]=='$') and not show_invisibale):
 			continue
+		abspath = os.path.join(path,x)
 
 		if(os.path.isfile(abspath)):
 			if(x==files[-1] and len(last_out)==0):
-				pre = pre2
+				l = list(pre)
+				l[-3] = '└'
+				pre = ''.join(l)
 			print(pre+x)
 		if(os.path.isdir(abspath)):
 			last_out.append(x)
 
 	for x in last_out:
+		nextpre= pre[0:-3]+'│    '+'├──'
 		abspath = os.path.join(path,x)
 		if(x==last_out[-1]):
-			pre = pre2
-		print(pre+'>'+x)
-		tree(abspath,deep+1)
+			# 如果是最后一个文件夹的时候，那和上一行的衔接就不应该用 ‘│’ 应该用空格。
+			nextpre = pre[0:-3]+'     '+'├──'
+			pre = pre[0:-3] + '└' + pre[-2:]
+			
+		print(pre+'>'+'<'+x+'>')
+		tree(abspath,deep=deep+1,maxdeep=maxdeep,pre=nextpre)
 
-tree(rootname,0,5)
+
+rootname = input('enter your dir path:')
+rootname = rootname.strip()
+maxdeep = input('enter max deep ')
+if maxdeep=='':
+	maxdeep = 5
+else:
+	maxdeep = int(maxdeep)
+if(os.path.exists(rootname) and os.path.isdir(rootname)):
+	tree(rootname,deep=0,maxdeep=maxdeep)
+
 
 '''
  ├ ─ ─ 
 
  └ ─ ─  
-
+│ 
  ├── aaaaa
     ├── aaa-2-inin
     ├── aaa-ininin
